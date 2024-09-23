6c360aba4e6d0e07ae3ff25289e7f13103447745
Rahul Ahuja <rahul@engs-30030.local>
2024-09-23 23:44:16 +0100

    updated shortest path function

--- graph_dir/calculate_shortest_path.py
+++ graph_dir/calculate_shortest_path.py
@@ -6,11 +6,18 @@ loaded_graph = igraph.Graph.Read_Pickle('graph_igraph.pkl')
 def shortest_path(departature: str, destination: str, g: igraph = loaded_graph) -> tuple:
     source = g.vs.select(city_names_eq=departature)
     target = g.vs.select(city_names_eq=destination)
-    shortest_path = g.get_shortest_paths(source[0], target[0], weights='travel_time')
+    shortest_path = g.get_shortest_paths(source[0], target[0], weights='travel_time')[0]
     short_path_rels = g.get_shortest_paths(source[0], target[0], weights='travel_time', output='epath')
-    short_path_distances = [g.es[edge]['travel_time'] for edge in short_path_rels]
-    shortest_travel_time = sum(short_path_distances[0])
+    short_path_distances = [g.es[edge]['travel_time'] for edge in short_path_rels][0]
+    shortest_travel_time = sum(short_path_distances)
 
-    return shortest_path, short_path_distances, shortest_travel_time
+    shortest_path_city = []
+    for city_idx in range(len(shortest_path)):
+        shortest_path_city.append(g.vs.select(city_ids_eq=shortest_path[city_idx])[0]['city_names'])
+
+
+
+
+    return shortest_path_city, short_path_distances, shortest_travel_time
 
 print(shortest_path('San Diego, CA', 'St. Johns, NL'))
