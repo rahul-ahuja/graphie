b362655261d9b6fced628e094bea2430e50c51cb
Rahul Ahuja <rahul@engs-30030.local>
2024-12-28 00:16:00 +0000

    add unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,17 @@ import igraph
 import pytest
 import pandas as pd
 
-#The fixture is created once per module that uses it and is destroyed at the end of the test session.
+#If the scope is module then the fixture is created once per module that uses it and is destroyed at the end of the test session.
+#If the scope is function then the fixture is destroyed at the end of the function call
+
 @pytest.fixture(scope="module")
 def pickled_graph():
     return igraph.Graph.Read_Pickle('graph_dir/graph_igraph.pkl')
 
-#destroyed at the end of the function call
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def nodes_df():
     return pd.read_csv('data/reachability-meta.csv')
 
-#destroyed at the end of the function call
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def edges_df():
     return pd.read_csv('data/reachability.csv', header=None)
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from graph_dir.calculate_shortest_path import shortest_path
 
 def test_num_nodes(pickled_graph, nodes_df):
     "Match the number of rows in the dataframe with node attributes and the number of nodes in the graphs"
@@ -17,3 +17,12 @@ def test_check_node_entry(pickled_graph, nodes_df):
     city_id_graph = pickled_graph.vs.select(city_names_eq=city_name)[0]['city_ids']
     city_id_df = nodes_df[nodes_df['name'] == city_name]['node_id'].values[0]
     assert city_id_graph == city_id_df
+
+def test_min_distance(edges_df, pickled_graph):
+    "Match the minimum time from the dataframes with the shortest time from the shortest path function"
+    min_time = min(edges_df[2])
+    depart_id, dest_id = edges_df[edges_df[2] == min(edges_df[2])][0].values
+    depart_name = pickled_graph.vs.select(city_ids_eq=depart_id)[0]['city_names']
+    dest_name = pickled_graph.vs.select(city_ids_eq=dest_id)[0]['city_names']
+    shortest_time = shortest_path(depart_name, dest_name, pickled_graph)[-1]
+    assert shortest_time == min_time
